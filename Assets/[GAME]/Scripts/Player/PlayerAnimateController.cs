cf3fa675a3db75efc763f0c777db892e657c8ab1
ozelonur <sumotoma_onur@hotmail.com>
2024-03-30 04:11:29 +0300

    Animator Update

--- Assets/[GAME]/Scripts/Player/PlayerAnimateController.cs
+++ Assets/[GAME]/Scripts/Player/PlayerAnimateController.cs
@@ -9,7 +9,6 @@ namespace _GAME_.Scripts.Player
         private Animator _animator;
         private static readonly int MoveX = Animator.StringToHash("MoveX");
         private static readonly int MoveZ = Animator.StringToHash("MoveZ");
-        private static readonly int Speed = Animator.StringToHash("Speed");
 
         private Vector2 _currentDirection = Vector2.zero;
         private static readonly int DieKey = Animator.StringToHash("Die");
@@ -29,32 +28,8 @@ namespace _GAME_.Scripts.Player
 
         public void Move(Vector2 direction)
         {
-            float threshold = 0.707107f;
-            float difference = 0.001f;
-
-            Vector2 targetDirection = direction;
-
-            if ((Mathf.Abs(direction.x) > (threshold - difference) &&
-                 Mathf.Abs(direction.x) < (threshold + difference)) &&
-                (Mathf.Abs(direction.y) > (threshold - difference) &&
-                 Mathf.Abs(direction.y) < (threshold + difference)))
-            {
-                targetDirection.x = Mathf.Sign(direction.x);
-                targetDirection.y = Mathf.Sign(direction.y);
-            }
-
-            if (direction.y < 0)
-            {
-                targetDirection.x *= -1;
-                _animator.SetFloat(Speed, -1);
-            }
-            else
-            {
-                _animator.SetFloat(Speed, 1);
-            }
-            
-            _currentDirection.x = Mathf.Lerp(_currentDirection.x, targetDirection.x, 15f * Time.deltaTime);
-            _currentDirection.y = Mathf.Lerp(_currentDirection.y, targetDirection.y, 15f * Time.deltaTime);
+            _currentDirection.x = Mathf.Lerp(_currentDirection.x, direction.x, 15f * Time.deltaTime);
+            _currentDirection.y = Mathf.Lerp(_currentDirection.y, direction.y, 15f * Time.deltaTime);
 
             _animator.SetFloat(MoveX, _currentDirection.x);
             _animator.SetFloat(MoveZ, _currentDirection.y);
